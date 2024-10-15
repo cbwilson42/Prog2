66dc4245e5a43cb4b1b354a1543c0e7d9be7c054
ZachJones42 <zjones42@tntech.edu>
2024-10-15 12:46:00 -0500

    Made basics for student class

---
+++ Student.h
@@ -0,0 +1,22 @@
+#ifndef Student_H
+#define Student_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student
+{
+private:
+
+string firstName;
+string lastName;
+string major;
+int credits;
+string *courses;
+
+public:
+
+
+};
+
+#endif
