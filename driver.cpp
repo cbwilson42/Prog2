8683afb6372569eef2ddd266ccdd54eefa84d0db
ZachJones42 <zjones42@tntech.edu>
2024-10-27 18:14:03 -0500

    Driver

---
+++ driver.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "Student.h"
+#include "Linkedlist.h"
+#include "listNode.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Making Student Management List.\n\n";
+    Student student1("Alex", "Walsh", "Electrical Engineer", 2.76);//Courses as 5th parameter
+
+}
